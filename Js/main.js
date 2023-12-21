f99103eab090611903c8fe4008308e527e29a50e
francescolazzarin <francesco.lazzarin1996@gmail.com>
2023-12-21 14:30:27 +0100

    <feat>Create le tre variabili nome,cognome e colore preferito,           alle quali viene associato il valore inserito dall'utente           tramite  prompt,succesivamente scritte nell'html

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+let nome=prompt("inserisci il tuo nome: ")
+let cognome=prompt("inserisci il tuo cognome: ")
+let fav_Color=prompt("inserisci il tuo colore preferito: ")
+
+document.writeln("<p>il tuo nome è: " + (nome) + (" ") + (cognome) , " </p>");
+document.writeln("<p>il tuo colore preferito è: " + (fav_Color), "</p>");
